dc4f2383bd17bbc157c933ff93b6300c5f773f3e
Ok <240349211+reconfirmok@users.noreply.github.com>
2026-08-25 07:34:59 +0300

    feat: docs

--- handler_chirps_retrieve.go
+++ handler_chirps_retrieve.go
@@ -1,11 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/reconfirmok/chirpy/internal/database"
+)
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
-	dbChrips, err := cfg.db.GetChirps(r.Context())
+	authorID, err := authorIDFromRequest(r)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Error Retrieving Chrips", err)
+		respondWithError(w, http.StatusBadRequest, "Invalid author ID", err)
+		return
+	}
+
+	var dbChrips []database.Chirp
+
+	if authorID != uuid.Nil {
+		dbChrips, err = cfg.db.GetChirpsByAuthor(r.Context(), authorID)
+	} else {
+		dbChrips, err = cfg.db.GetChirps(r.Context())
+	}
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error retrieving chrips", err)
 		return
 	}
 
@@ -21,4 +38,17 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 	}
 
 	respondWithJSON(w, http.StatusOK, chirpsList)
+}
+
+func authorIDFromRequest(r *http.Request) (uuid.UUID, error) {
+	authorIDStr := r.URL.Query().Get("author_id")
+	if authorIDStr == "" {
+		return uuid.Nil, nil
+	}
+
+	authorId, err := uuid.Parse(authorIDStr)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return authorId, nil
 }
